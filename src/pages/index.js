8c144c41e511967e8e0185bd5289084f5e686a85
Dejan85 <xypnotica@gmail.com>
2021-04-29 18:37:47 +0200

    added styles to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import { header, btn } from "../styles/home.module.css"
 
 const Home = () => {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & deploy</h3>
           <p>Develop in Gutsby</p>
+          <Link to="/projects" className={btn}>
+            My Projects
+          </Link>
         </div>
       </section>
     </Layout>
