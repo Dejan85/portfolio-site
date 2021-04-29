1a6b4ae2a3cb984f5888931d106f30a599792847
Dejan85 <xypnotica@gmail.com>
2021-04-29 21:09:02 +0200

    created css for projecst

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,34 +1,48 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from "../../components/Layout"
-import { portfolio } from "../../styles/projects.module.css"
+import {
+  portfolio,
+  projects as projectsStyle,
+} from "../../styles/projects.module.css"
 
-const Projects = () => {
-  const data = useStaticQuery(graphql`
-    query MyQuery {
-      allMarkdownRemark {
-        nodes {
-          frontmatter {
-            slug
-            stack
-            title
-          }
-          id
-        }
-      }
-    }
-  `)
-
-  console.log("test", data)
+const Projects = props => {
+  const { data } = props
+  const projects = data.allMarkdownRemark.nodes
 
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Website I've created</h3>
+        <div className={projectsStyle}>
+          {projects.map(project => (
+            <Link key={project.id} to={`/projects/${project.frontmatter.slug}`}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stuck}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
 
 export default Projects
+
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
