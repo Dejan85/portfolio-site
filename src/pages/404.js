383fa14df4a9f748dd6d2dcbfc60ef6e6973c470
Dejan85 <xypnotica@gmail.com>
2021-04-29 16:49:38 +0200

    created basic routing

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Sry that page doest exist</p>
+    </div>
+  )
+}
+
+export default NotFound
